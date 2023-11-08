abc479c4230742fb3c9d6a1140ccdea70fcb8be5
Anna <Zvereva.krsk@gmail.com>
2023-11-07 20:13:29 -0500

    added text story because the part doen't complit

--- CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -23,4 +23,6 @@ func chapterOne() {
     //    print ("Once upon a time a young and talent boy named  \(swiftBoysName) who was born by \(myFirstFunction()) famous company \(companyName()) in \(myFirstFunction()) \(mySecondFunction()) ")
     //    print ("Every day \(swiftBoysName) was playing in \(myFirstFunction()  \(mySecondFunction()) solving problems, and learning \(myFirstFunction()) the new languange")
     //}
+    
+    print("Once upon a time a young and talented boy named Jimmy Swift, who was born by the famous company “Apple” in the technology world. He had happy and carefree life. He lived with his father Objective C and grandfather C. Also, he had close friends who always were with him Tommy Python and Sarah Ruby. Every day Jimmy was playing in the technology world solving “kids” problems, and spending time enjoying his happy live.")
 }
